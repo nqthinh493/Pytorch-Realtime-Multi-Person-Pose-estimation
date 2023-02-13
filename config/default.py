438345f26d8b3d5c13d5b7573b119fdd6598493f
nqthinh493 <nqthinh493@gmail.com>
2023-02-14 00:24:16 +0800

    upload project code

---
+++ config/default.py
@@ -0,0 +1,35 @@
+from yacs.config import CfgNode as CN
+
+_C = CN()
+
+_C.COCO2017_DATASET = CN()
+_C.COCO2017_DATASET.TRAIN = './COCO2017/train2017'
+_C.COCO2017_DATASET.TEST = './COCO2017/test2017'
+_C.COCO2017_DATASET.VAL = './COCO2017/val2017'
+_C.COCO2017_DATASET.TRAIN_ANNOTATIONS = './COCO2017/annotations2017/person_keypoints_train2017.json'
+_C.COCO2017_DATASET.VAL_ANNOTATIONS = './COCO2017/annotations2017/person_keypoints_val2017.json'
+
+_C.INFANT_DATASET = CN()
+_C.INFANT_DATASET.TRAIN = './COCO2017/train2017'
+_C.INFANT_DATASET.TEST = './COCO2017/test2017'
+_C.INFANT_DATASET.VAL = './COCO2017/val2017'
+_C.INFANT_DATASET.TRAIN_ANNOTATIONS = './COCO2017/annotations2017/person_keypoints_train2017.json'
+_C.INFANT_DATASET.VAL_ANNOTATIONS = './COCO2017/annotations2017/person_keypoints_val2017.json'
+
+_C.TRAINING = CN()
+_C.TRAINING.DATASET = None
+_C.TRAINING.EPOCH = 3
+_C.TRAINING.LOG_FOLDER = './Results'
+_C.TRAINING.PRETRAINED_WEIGHT = './models/models_weight/mobilenet_sgd_68.848.pth.tar'
+_C.TRAINING.TRANSFER_WEIGHT = None
+
+_C.TEMP = CN()
+_C.TEMP.PREPARED_DATA = './temp/prepared_data'
+
+
+
+def get_cfg_defaults():
+  """Get a yacs CfgNode object with default values for my_project."""
+  # Return a clone so that the defaults will not be altered
+  # This is for the "local variable" use pattern
+  return _C.clone()
---
+++ config/path_cfg.py
@@ -0,0 +1,31 @@
+from config import get_cfg_defaults, update_config
+import os
+
+cfg =  get_cfg_defaults()
+cfg =  update_config(cfg, './config/params.yaml')
+
+
+
+def select_dataset(dataset):
+    if dataset == 'COCO':
+        TRAIN_PATH = cfg.COCO2017_DATASET.TRAIN
+        VAL_PATH = cfg.COCO2017_DATASET.VAL
+        TEST_PATH = cfg.COCO2017_DATASET.TEST
+        TRAIN_ANNO = cfg.COCO2017_DATASET.TRAIN_ANNOTATIONS
+        VAL_ANNO = cfg.COCO2017_DATASET.TRAIN_ANNOTATIONS
+        return TRAIN_PATH, VAL_PATH, TEST_PATH, TRAIN_ANNO, VAL_ANNO
+    elif dataset == 'INFANT':
+        TRAIN_PATH = cfg.INFANT_DATASET.TRAIN
+        VAL_PATH = cfg.INFANT_DATASET.VAL
+        TEST_PATH = cfg.INFANT_DATASET.TEST
+        TRAIN_ANNO = cfg.INFANT_DATASET.TRAIN_ANNOTATIONS
+        VAL_ANNO = cfg.INFANT_DATASET.TRAIN_ANNOTATIONS
+        return TRAIN_PATH, VAL_PATH, TEST_PATH, TRAIN_ANNO, VAL_ANNO
+    
+    else:
+        print('Please add dataset and selection in config!!!')
+        
+dataset = cfg.TRAINING.DATASET
+PREPARED_TRAIN = os.path.join(cfg.TEMP.PREPARED_DATA, f'prepared_train_annotation_{dataset}.pkl')
+PREPARED_VAL = os.path.join(cfg.TEMP.PREPARED_DATA, f'val_subset_{dataset}.json')
+TRAIN_PATH, VAL_PATH, TEST_PATH, TRAIN_ANNO, VAL_ANNO = select_dataset(dataset)
